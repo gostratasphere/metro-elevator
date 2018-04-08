afdfb00a91bd50943c6deaace2c330f125ce673f
Ryan Personal <rchillard@Rapier.local>
2018-04-08 13:47:19 -0400

    Added additional styling, dependencies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Expressly' });
+});
+
+router.post('/form', function(req, res, next) {
+  console.log(req.body)
+  res.send(req.body.station)
 });
 
 module.exports = router;
